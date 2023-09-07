306256135210973ea69441d8dfcc19427c127f30
RishabhSinghal04 <rishabhsinghal2302@gmail.com>
2023-09-07 17:29:14 +0530

    added and removed some functions and modified them

--- Linked List/Insertion_Deletion.cpp
+++ Linked List/Insertion_Deletion.cpp
@@ -11,44 +11,49 @@ int main()
 
     Linked_List<uint32_t> whole_numbers;
 
-    whole_numbers.push_back(2);
-    whole_numbers.push(0);
-    whole_numbers.push_back(3);
-
-    Node<uint32_t> *n = whole_numbers.get_head();
-    whole_numbers.insert_after(n, 1);
-
-    for(uint32_t num = 4; num < 10; ++num)
+    try
     {
-        whole_numbers.push_back(num);
+        whole_numbers.push_back(2);
+        whole_numbers.push(0);
+        whole_numbers.push_back(3);
+
+        whole_numbers.insert_at(1, 1);
+
+        for (uint32_t num = 4; num < 10; ++num)
+        {
+            whole_numbers.push_back(num);
+        }
+
+        whole_numbers.insert_at(10, 111);
+        // whole_numbers.insert_at(18, 1111);
+        whole_numbers.display();
+        cout << '\n';
+
+        whole_numbers.delete_first_element();
+        whole_numbers.display();
+        cout << '\n';
+
+        whole_numbers.delete_last_element();
+        whole_numbers.display();
+        cout << '\n';
+
+        whole_numbers.delete_at(1);
+        whole_numbers.display();
+        cout << '\n';
+
+        whole_numbers.delete_at(6);
+        whole_numbers.display();
+        cout << '\n';
+
+        whole_numbers.delete_element(5);
+        whole_numbers.delete_element(9);
+        whole_numbers.display();
+        cout << "\nNumber of element(s) : " << whole_numbers.get_size() << '\n';
     }
-
-    Node<uint32_t> m(10U);
-
-    whole_numbers.push_back(m.data);
-    whole_numbers.display();
-    cout << '\n';
-
-    whole_numbers.delete_first_node();
-    whole_numbers.display();
-    cout << '\n';
-
-    whole_numbers.delete_last_node();
-    whole_numbers.display();
-    cout << '\n';
-
-    Node<uint32_t> *d = whole_numbers.get_head()->next;
-    whole_numbers.delete_after(d);
-    whole_numbers.display();
-    cout << '\n';
-    
-    whole_numbers.delete_at(6);
-    whole_numbers.display();
-    cout << '\n';
-
-    whole_numbers.delete_node(5);
-    whole_numbers.display();
-    cout << '\n';
+    catch (std::exception &ex)
+    {
+        cout << "Exception Caught : " << ex.what() << '\n';
+    }
 
     getch();
     return 0;
--- Linked List/linked_list.h
+++ Linked List/linked_list.h
@@ -1,199 +1,232 @@
 #pragma once
 
-#include <iostream>
+#include <sstream>
+#include <cmath>
 
 namespace ll
 {
     template <typename T>
-    struct Node
-    {
-        T data;
-        Node<T> *next{};
-
-        Node()
-            : Node({}) {}
-        Node(const T &data)
-            : data{data}, next{nullptr} {}
-    };
-
-    template <typename T>
     class Linked_List
     {
-        Node<T> *head;
+        template <typename V>
+        struct Node
+        {
+            V data;
+            Node<V> *next = nullptr;
+
+            Node()
+                : Node({}) {}
+            Node(const V &data)
+                : data{data}, next{nullptr} {}
+        };
+        Node<T> *head = nullptr;
+        size_t size = 0ULL;
+        const std::string message_empty{"List is Empty"};
 
     public:
         Linked_List()
-            : head{NULL} {}
+            : head{nullptr}, size{0ULL} {}
 
-        ~Linked_List()
+        ~Linked_List() { clear(); }
+
+        bool empty() const { return head == nullptr; }
+
+        /* __Getters__ */
+        const T get_size() const { return size; }
+
+        const T get_head_data() const
         {
-            Node<T> *next{};
-            while (head != NULL)
+            if (empty())
             {
-                next = head->next;
-                delete head;
-                head = next;
+                throw std::runtime_error(message_empty);
             }
-            std::cout << "\nEXIT\n";
+            return head->data;
         }
 
-        // Getter
-        Node<T> *get_head() const
-        {
-            return head;
-        }
-
-        // Add a new node to the front of the list
+        /* _Add Nodes_ */
         void push(const T &data)
         {
             Node<T> *new_node = new Node<T>(data);
             new_node->next = head;
             head = new_node;
+            ++size;
         }
 
-        // Add a new node to the back of the list
         void push_back(const T &data)
         {
-            Node<T> *new_node = new Node<T>(data);
-            Node<T> *last_node = head;
-            new_node->next = NULL;
-
-            // If list is empty, add new node at the front
-            if (head == NULL)
+            if (empty())
             {
-                head = new_node;
-                return;
+                return push(data);
             }
 
-            // Get last node and add new node at last
+            Node<T> *new_node = new Node<T>(data);
+            Node<T> *last_node = head;
+            new_node->next = nullptr;
+
             while (last_node->next != nullptr)
             {
                 last_node = last_node->next;
             }
             last_node->next = new_node;
+            ++size;
         }
 
-        // Add a new node in between
-        void insert_after(Node<T> *previous_node, const T &data)
+        void insert_at(const size_t &index, const T &data)
         {
-            if (previous_node == NULL)
+            if (empty())
             {
-                std::cerr << "\nGiven previous node cannot be null\n";
-                return;
+                throw std::runtime_error(message_empty);
+            }
+            if (index > size)
+            {
+                throw std::runtime_error("Out of Range");
+            }
+            if (index == 0ULL)
+            {
+                return push(data);
+            }
+            if (index == size)
+            {
+                return push_back(data);
             }
 
-            Node<T> *new_node = new Node<T>();
-            new_node->data = data;
-            new_node->next = previous_node->next;
-            previous_node->next = new_node;
-        }
+            Node<T> *node = new Node<T>(data);
+            Node<T> *prev_node = head;
 
-        // Add a new node at given index
-        void insert_at(const size &index, const T &data)
-        {
-        }
-
-        void delete_first_node()
-        {
-            if (head == NULL)
+            for (size_t i = 1ULL; i < index; ++i)
             {
-                return;
+                prev_node = prev_node->next;
+            }
+
+            node->next = prev_node->next;
+            prev_node->next = node;
+            ++size;
+        }
+
+        /* _Delete Nodes_ */
+        void clear()
+        {
+            while (head != nullptr && head->next != nullptr)
+            {
+                delete_next_node(head);
+            }
+            if (head != nullptr)
+            {
+                free_memory(head);
+                head = nullptr;
+            }
+            size = 0ULL;
+        }
+
+        void delete_first_element()
+        {
+            if (empty())
+            {
+                throw std::runtime_error(message_empty);
             }
 
             Node<T> *first_node = head;
             head = first_node->next;
-            delete first_node;
+            --size;
+            free_memory(first_node);
         }
 
-        void delete_last_node()
+        void delete_last_element()
         {
-            if (head == NULL)
+            if (size < 2ULL)
             {
-                return;
+                return delete_first_element();
             }
 
             Node<T> *second_last_node = head;
-            Node<T> *last_node = head->next;
 
-            while (last_node->next != nullptr)
+            while (second_last_node->next->next != nullptr)
             {
                 second_last_node = second_last_node->next;
-                last_node = last_node->next;
             }
-            second_last_node->next = nullptr;
-            delete last_node;
-        }
-
-        void delete_after(Node<T> *previous_node)
-        {
-            if (previous_node == NULL)
-            {
-                std::cerr << "\nGiven previous node cannot be null\n";
-                return;
-            }
-
-            Node<T> *next_node = previous_node->next;
-            previous_node->next = next_node->next;
-            delete next_node;
+            delete_next_node(second_last_node);
         }
 
         void delete_at(const size_t &index)
         {
-            if (head == NULL)
+            if (empty())
             {
-                return;
+                throw std::runtime_error(message_empty);
+            }
+            if (index == 0ULL)
+            {
+                return delete_first_element();
             }
 
-            Node<T> *temp = head->next;
-            Node<T> *previous_node = head;
+            Node<T> *prev_node = head;
 
-            for (size_t i = 0; i < index - 1; ++i)
+            for (size_t i = 1; i < index; ++i)
             {
-                temp = temp->next;
-                previous_node = previous_node->next;
+                prev_node = prev_node->next;
             }
-            previous_node->next = temp->next;
-            delete temp;
+            delete_next_node(prev_node);
         }
 
-        // Delete first node with the given key
-        void delete_node(const T &key)
+        void delete_element(const T &key)
         {
-            Node<T> *temp = head;
-            Node<T> *previous_node = NULL;
-
-            if (temp != NULL && temp->data == key)
+            if (empty())
             {
-                head = temp->next;
-                temp = NULL;
+                throw std::runtime_error(message_empty);
+            }
+
+            auto equal = []<typename V>(const V &data, const V &key)
+            {
+                const double PRECISION = 1e-2;
+                return (std::is_floating_point_v<V>)
+                           ? fabs(data - key) < PRECISION
+                           : data == key;
+            };
+
+            if (equal(head->data, key))
+            {
+                return delete_first_element();
+            }
+
+            Node<T> *prev_node = head;
+
+            while (prev_node->next != nullptr && !equal(prev_node->next->data, key))
+            {
+                prev_node = prev_node->next;
+            }
+
+            if (prev_node->next == nullptr)
+            {
                 return;
             }
-
-            while (temp != NULL && temp->data != key)
-            {
-                previous_node = temp;
-                temp = temp->next;
-            }
-
-            if (temp == NULL)
-            {
-                return;
-            }
-
-            previous_node->next = temp->next;
-            delete temp;
+            delete_next_node(prev_node);
         }
 
         void display()
         {
+            std::ostringstream oss;
             Node<T> *current = head;
-            while (current != NULL)
+            while (current != nullptr)
             {
-                std::cout << current->data << "-> ";
+                oss << current->data << "-> ";
                 current = current->next;
             }
-            std::cout << "\b\b\b  \b\b";
-            delete current;
+            // Remove the last arrow
+            std::cout << oss.str().substr(0, oss.str().length() - 3);
+        }
+
+    private:
+        void delete_next_node(Node<T> *prev_node)
+        {
+            Node<T> *node = prev_node->next;
+            prev_node->next = node->next;
+            --size;
+            free_memory(node);
+        }
+
+        void free_memory(Node<T> *node)
+        {
+            delete node;
+            node = nullptr;
         }
     };
 }
